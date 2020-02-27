b94a08d77aadadda46d6cdb5ba09c050d283ef97
nari19 <kazuki7793996@gmail.com>
2020-02-27 09:19:07 +0900

    arrow func

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,19 +1,18 @@
 const main = (): void => {
-  const randomNum: number = Math.floor(Math.random() * 99) + 1;
   const btn: HTMLInputElement = <HTMLInputElement>(document.getElementById('btn'));
-  btn.addEventListener('click', btnClickEvent, false);
 
-  // フォームの値を取得
-  function btnClickEvent(): void {
+  btn.addEventListener('click', (): void => {
     const numberValue: number = document.forms[0].number.value;
     if (isNaN(numberValue)) {
       alert(`数字で入力してください`);
     } else if(numberValue > 100 || numberValue < 1) {
       alert(`1から100の数字を入力してください`);
     } else {
+      const randomNum: number = Math.floor(Math.random() * 99) + 1;
       alert(randomNum);
     }
-  }
+  });
+
 };
 
 main();
