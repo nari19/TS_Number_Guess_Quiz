69a8e6a3abf8e25fedb801f5f2062913c563d6ef
nari19 <kazuki7793996@gmail.com>
2020-02-27 19:11:30 +0900

    残り回数の表示

--- src/dist/index.js
+++ src/dist/index.js
@@ -1,7 +1,10 @@
 "use strict";
 (() => {
-    const randomNum = Math.floor(Math.random() * 100) + 1;
+    let randomNum = Math.floor(Math.random() * 100) + 1;
     console.log(randomNum);
+    let clickTimes = 10;
+    const clickTimesText = document.getElementById('clickTimesText');
+    clickTimesText.innerHTML = `${clickTimes}`;
     const btn = (document.getElementById('btn'));
     btn.onclick = () => {
         const numberValue = document.forms[0].number.value;
@@ -12,14 +15,18 @@
             alert("1から100の数字を入力してください");
         }
         else {
+            clickTimes -= 1;
+            clickTimesText.innerHTML = `${clickTimes}`;
             if (numberValue > randomNum) {
-                alert("大きいよ!!");
+                alert("答えよりも小さいよ!!");
             }
             else if (numberValue < randomNum) {
-                alert("小さいよ!!");
+                alert("答えよりも大きいよ!!");
             }
             else {
-                alert("正解!!");
+                alert("正解!! (新しい数字を生成します)");
+                randomNum = Math.floor(Math.random() * 100) + 1;
+                console.log(randomNum);
             }
         }
     };
--- src/script/index.ts
+++ src/script/index.ts
@@ -1,22 +1,31 @@
 ((): void => {
-  const randomNum: number = Math.floor(Math.random() * 100) + 1;
+  let randomNum: number = Math.floor(Math.random() * 100) + 1;
   console.log(randomNum); 
-  const btn: HTMLInputElement = <HTMLInputElement>(document.getElementById('btn'));
 
-  btn.onclick = () => {
-    const numberValue = document.forms[0].number.value;
+  let clickTimes: number = 10;
+  const clickTimesText: HTMLElement = <HTMLElement>document.getElementById('clickTimesText');
+  clickTimesText.innerHTML = `${clickTimes}`;
+
+  const btn: HTMLInputElement = <HTMLInputElement>(document.getElementById('btn'));
+  btn.onclick = (): void => {
+    const numberValue: number = document.forms[0].number.value;
     if (isNaN(numberValue)) {
       alert("数字で入力してください");
-    } else if(numberValue > 100 || numberValue < 1) {
+    } else if (numberValue > 100 || numberValue < 1) {
       alert("1から100の数字を入力してください");
     } else {
+      clickTimes -= 1;
+      clickTimesText.innerHTML = `${clickTimes}`;
       if (numberValue > randomNum) {
-        alert("大きいよ!!");
-      } else if(numberValue < randomNum) {
-        alert("小さいよ!!");
+        alert("答えよりも小さいよ!!");
+      } else if (numberValue < randomNum) {
+        alert("答えよりも大きいよ!!");
       } else {
-        alert("正解!!");
+        alert("正解!! (新しい数字を生成します)");
+       randomNum = Math.floor(Math.random() * 100) + 1;
+       console.log(randomNum);
       }
     }
   };
+
 })();
