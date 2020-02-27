50a84eebc8d81351d62fb13f7de80e2af6a1f91b
nari19 <kazuki7793996@gmail.com>
2020-02-27 10:51:16 +0900

    即時関数

--- src/dist/index.js
+++ src/dist/index.js
@@ -1,21 +1,27 @@
 "use strict";
-const main = () => {
+(() => {
     const randomNum = Math.floor(Math.random() * 99) + 1;
+    console.log(randomNum);
     const btn = (document.getElementById('btn'));
-    btn.addEventListener('click', btnClickEvent, false);
-    // フォームの値を取得
-    function btnClickEvent() {
+    btn.addEventListener('click', function () {
         const numberValue = document.forms[0].number.value;
         if (isNaN(numberValue)) {
-            alert(`数字で入力してください`);
+            alert("数字で入力してください");
         }
         else if (numberValue > 100 || numberValue < 1) {
-            alert(`1から100の数字を入力してください`);
+            alert("1から100の数字を入力してください");
         }
         else {
-            alert(randomNum);
+            if (numberValue > randomNum) {
+                alert("大きいよ!!");
+            }
+            else if (numberValue < randomNum) {
+                alert("小さいよ!!");
+            }
+            else {
+                alert("正解!!");
+            }
         }
-    }
-};
-main();
+    }, false);
+})();
 //# sourceMappingURL=index.js.map
--- src/script/index.ts
+++ src/script/index.ts
@@ -1,18 +1,22 @@
-const main = (): void => {
+((): void => {
+  const randomNum: number = Math.floor(Math.random() * 99) + 1;
+  console.log(randomNum);
   const btn: HTMLInputElement = <HTMLInputElement>(document.getElementById('btn'));
 
-  btn.addEventListener('click', (): void => {
+  btn.addEventListener('click', function() {
     const numberValue: number = document.forms[0].number.value;
     if (isNaN(numberValue)) {
-      alert(`数字で入力してください`);
+      alert("数字で入力してください");
     } else if(numberValue > 100 || numberValue < 1) {
-      alert(`1から100の数字を入力してください`);
+      alert("1から100の数字を入力してください");
     } else {
-      const randomNum: number = Math.floor(Math.random() * 99) + 1;
-      alert(randomNum);
+      if (numberValue > randomNum) {
+        alert("大きいよ!!");
+      } else if(numberValue < randomNum) {
+        alert("小さいよ!!");
+      } else {
+        alert("正解!!");
+      }
     }
-  });
-
-};
-
-main();
+  }, false);
+})();
